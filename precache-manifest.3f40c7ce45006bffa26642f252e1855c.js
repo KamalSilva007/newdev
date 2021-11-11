3b09b4db71262e796185cd6b2ffa3d40a4963a46
Yasas4D <yasassandeepa007@gmail.com>
2021-11-11 18:12:10 +0530

    Updates

---
+++ precache-manifest.3f40c7ce45006bffa26642f252e1855c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b3393742fa18455714d3df0e0388b1",
+    "url": "/newdev/index.html"
+  },
+  {
+    "revision": "663426b899c115a5585b",
+    "url": "/newdev/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "a12d2b01b3e73be17992",
+    "url": "/newdev/static/css/main.c4bd527b.chunk.css"
+  },
+  {
+    "revision": "663426b899c115a5585b",
+    "url": "/newdev/static/js/2.e1577c64.chunk.js"
+  },
+  {
+    "revision": "de0a1713895ef548baca42cd10aace43",
+    "url": "/newdev/static/js/2.e1577c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12d2b01b3e73be17992",
+    "url": "/newdev/static/js/main.c69d9382.chunk.js"
+  },
+  {
+    "revision": "270c231288c033a7508b",
+    "url": "/newdev/static/js/runtime-main.70c90d98.js"
+  }
+]);
